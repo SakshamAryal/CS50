0319f03106c93118a6a9aee5f11d7c7a863e347e
VanEphson <134315357+VanEphson@users.noreply.github.com>
2024-04-18 16:52:29 -0400

    FINALLY DONE

--- credit.c
+++ credit.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int is_mastercard(long mas);
+bool is_mastercard(long long mas, bool luhn);
 
-int is_visa(long vi);
+bool is_visa(long long vi, bool luhn);
 
-int is_american(long ame);
+bool is_american(long long ame, bool luhn);
 
-int is_visa13(long vi);
+bool is_visa13(long long vi, bool luhn);
 
 int main(void)
 {
 
     int comparision;
     long long card;
-
     do
     {
         printf("Number: ");
         scanf("%lld", &card);
-    } while (card < 9999999999);
+    } while (card < 999999999);
 
-    long k = 10;
-    long rem = 0, total1 = 0, total2 = 0, final, check = 10;
-    long mastercard = is_mastercard(card);
-    long visa = is_visa(card);
-    long amex = is_american(card);
-    long visa2 = is_visa13(card);
+    if (card < 999999999999)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    long long card2 = card, k = 10, rem = 0, total1 = 0, total2 = 0, final, check = 10;
     int card_num = 0;
+    bool luhn;
 
     if (card < 9999999999999 && card > 999999999999)
     {
@@ -35,10 +37,6 @@ int main(void)
     else if (card < 999999999999999 && card > 9999999999999)
     {
         card_num = 15;
-    }
-    else if (card < 999999999999)
-    {
-        printf("INVALID\n");
     }
     else
     {
@@ -70,22 +68,29 @@ int main(void)
 
     if (final % check == 0)
     {
-        if (mastercard < 56 && mastercard > 50)
-        {
-            printf("MASTERCARD\n");
-        }
-        else if (amex == 34 || amex == 37)
-        {
-            printf("AMEX\n");
-        }
-        else if (visa == 4)
-        {
-            printf("VISA\n");
-        }
-        else
-        {
-            printf("INVALID\n");
-        }
+        luhn = true;
+    }
+    else
+    {
+        luhn = false;
+    }
+
+    bool mastercard = is_mastercard(card2, luhn);
+    bool visa = is_visa(card2, luhn);
+    bool amex = is_american(card2, luhn);
+    bool visa2 = is_visa13(card2, luhn);
+
+    if (mastercard == true)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if (visa == true || visa2 == true)
+    {
+        printf("VISA\n");
+    }
+    else if (amex == true)
+    {
+        printf("AMEX\n");
     }
     else
     {
@@ -93,37 +98,82 @@ int main(void)
     }
 }
 
-int is_mastercard(long mas)
+bool is_mastercard(long long mas, bool luhn)
 {
+    bool is_mas;
     if (mas > 999999999999999)
+    {
         mas = mas / 100000000000000;
-    return mas;
+    }
+    if (mas > 50 && mas < 56 && luhn == true)
+    {
+        is_mas = true;
+    }
+    else
+    {
+        is_mas = false;
+    }
+    return is_mas;
 }
 
-int is_visa13(long vi)
+bool is_visa13(long long vi, bool luhn)
 {
+    bool is_vis13;
     if (vi > 999999999999 && vi < 99999999999999)
     {
-        vi = vi / 100000000000;
+        vi = vi / 1000000000000;
     }
-    return vi;
+    if (vi == 4 && luhn == true)
+    {
+        is_vis13 = true;
+    }
+    else
+    {
+        is_vis13 = false;
+    }
+    return is_vis13;
 }
 
-int is_visa(long vis)
+bool is_visa(long long vis, bool luhn)
 {
+    bool is_vis16;
     if (vis > 999999999999999)
     {
-        vis = vis / 100000000000000;
+        vis = vis / 1000000000000000;
     }
-    return vis;
+    if (vis == 4 && luhn == true)
+    {
+        is_vis16 = true;
+    }
+    else
+    {
+        is_vis16 = false;
+    }
+    return is_vis16;
 }
 
-int is_american(long ame)
+bool is_american(long long ame, bool luhn)
 {
+    bool is_ame;
     if (ame > 99999999999999)
     {
-        ame = ame / 100000000000000;
+        ame = ame / 10000000000000;
+    }
+    if (ame == 32 || ame == 37)
+    {
+        if (luhn == true)
+        {
+            is_ame = true;
+        }
+        else
+        {
+            is_ame = false;
+        }
+    }
+    else
+    {
+        is_ame = false;
     }
 
-    return ame;
+    return is_ame;
 }
